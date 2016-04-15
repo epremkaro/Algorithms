1b673e25bb3e38f991a4997260492c6fc594aa77
exit <quit>
2016-04-15 14:23:03 +0300

    added insertion sort

---
+++ src/algorithms/sort/InsertionSort.java
@@ -0,0 +1,28 @@
+package algorithms.sort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1, 123, 455, 6565, 4, 11, 0, 111};
+		
+		System.out.println(Arrays.toString(arr));
+		insertionSort(arr);
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+	private static void insertionSort(int[] arr) {
+		for (int out = 1; out < arr.length; out++){
+			for(int in = out ; in > 0 ; in--){
+				if (arr[in] < arr[in-1]){
+					int tmp = arr[in];
+					arr[in] = arr[in-1];
+					arr[in-1] = tmp;
+				}
+			}
+		}
+	}
+}
